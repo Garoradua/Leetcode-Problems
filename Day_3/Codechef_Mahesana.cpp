659e46241c2e3e71fc575fb2f84a9cc8d7924f90
Gourav Arora <gouravarora035@gmail.com>
2024-01-02 13:00:10 +0530

    Codechef mahesana

--- Day_3/Codechef_Mahesana.cpp
+++ Day_3/Codechef_Mahesana.cpp
@@ -2,23 +2,20 @@
 using namespace std;
 int main(){
 	cout << "Total no of soldiers :";
-	int N;
+	int N, lucky =0, unlucky =0;
 	cin >> N;
 	cout << "Enter the weapons of each soldier \n";
 	int arr[N];
-	for(int i=0; i<N; i++){
+	for(int i=0; i<N; i++)
 		cin >> arr[i];
-	}
-	int even=0;
-	int odd=0;
 	for(int i=0; i<N; i++){
 		if(arr[i]%2==0){
-			even++;
+			lucky++;
 		}else{
-			odd++;
+			unlucky++;
 		}
 	}
-	if(even>odd){
+	if(lucky>unlucky){
 		cout << "ready for battle";
 	}else{
 		cout << "Not ready";
