ddb184d3649d738e33c69de7fbf7e368397622bf
Gourav Arora <gouravarora035@gmail.com>
2024-01-06 22:02:43 +0530

    Sliding Window

---
+++ Day_6/Sliding_Window.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	 cin >> n;
+	 int arr[n];
+	 for(int i=0; i<n; i++){
+	 	cin >> arr[i];
+	 }
+	 int k;
+	 cin >> k;
+
+	 int sum =0, max=-1;
+	 for(int i=0; i<k; i++){
+	 	sum += arr[i];
+	 }
+	 max = sum;
+	 for(int i=1; i<=n-k; i++){
+	 	sum = sum-arr[i-1]+arr[i+k-1];
+	 	// cout << sum  << " ";
+	 	if(sum  > max){
+	 		max = sum;
+	 	}
+	 } 
+	 cout << max;
+}
