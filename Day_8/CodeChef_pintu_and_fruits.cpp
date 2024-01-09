02f3cefeef11b7bceb3dac847b88e8cc8f10f72c
Gourav Arora <gouravarora035@gmail.com>
2024-01-09 23:02:45 +0530

    Codechef pintu and fruits

--- Day_8/CodeChef_pintu_and_fruits.cpp
+++ Day_8/CodeChef_pintu_and_fruits.cpp
@@ -12,15 +12,21 @@ int main(){
 		int Min[M+1] = {0};
 		cout << "Enter all the basket" <<endl;
 		for(int i=0; i<N; i++){
-			cin >> basket[N];
+			cin >> basket[i];
 		}
 		for(int i=0; i<N; i++){
-			cin >> Price[N];
+			cin >> Price[i];
 		}
+		cout << "Hello";
+		int basket_number, pp;
 		for(int i=0; i<N; i++){
-			int basket_number = basket[i];
-			int pp = Price[i];
+			cout << "Hey";
+			basket_number = basket[i];
+			pp = Price[i];
+			cout << "basket number" << basket_number << endl;
+			cout <<  "price" << pp <<endl;
 			Min[basket_number] += pp;
+			cout << basket_number << " " << Min[basket_number] << endl ;
 		}
 		int min_amount = INT_MAX;
 		for(int i=1; i<=M; i++){
