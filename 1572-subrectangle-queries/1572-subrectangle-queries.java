5dee67bcdca8fece476d9d447695f68674d48805
Gourav Arora <gouravarora035@gmail.com>
2024-08-07 23:45:29 +0530

    Time: 20 ms (100%), Space: 47.9 MB (18.23%) - LeetHub

--- 1572-subrectangle-queries/1572-subrectangle-queries.java
+++ 1572-subrectangle-queries/1572-subrectangle-queries.java
@@ -1,19 +1,19 @@
 class SubrectangleQueries {
-    int[][] arr;
+    int[][] rectangle;
     public SubrectangleQueries(int[][] rectangle) {
-        arr = rectangle;
+        this.rectangle = rectangle;
     }
     
     public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
         for(int i=row1; i<=row2; i++){
             for(int j=col1; j<=col2; j++){
-                arr[i][j] = newValue;
+                this.rectangle[i][j] = newValue;
             }
         }
     }
     
     public int getValue(int row, int col) {
-        return arr[row][col];
+        return this.rectangle[row][col];
     }
 }
 
