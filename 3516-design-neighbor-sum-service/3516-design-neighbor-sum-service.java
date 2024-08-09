049c95840fb3017f02b6d79abd5f4188e3f3154d
Gourav Arora <gouravarora035@gmail.com>
2024-08-09 22:02:51 +0530

    Time: 20 ms (14.2%), Space: 45.3 MB (68.82%) - LeetHub

---
+++ 3516-design-neighbor-sum-service/3516-design-neighbor-sum-service.java
@@ -0,0 +1,47 @@
+class NeighborSum {
+    int[][] arr;
+    int row;
+    int column;
+    public NeighborSum(int[][] grid) {
+        arr = grid;
+        row = arr.length;
+        column = arr[0].length;
+    }
+    
+    public int adjacentSum(int value) {
+        int sum = 0;
+        for(int i=0; i<row; i++){
+            for(int j=0; j<column; j++){
+                if(arr[i][j]==value){
+                    if(j-1>=0) sum += arr[i][j-1];
+                    if(j+1 < column) sum += arr[i][j+1];
+                    if(i-1 >=0) sum += arr[i-1][j];
+                    if(i+1 < row) sum += arr[i+1][j];
+                }
+            }
+        }
+        return sum;
+    }
+    
+    public int diagonalSum(int value) {
+        int sum = 0;
+        for(int i=0; i<row; i++){
+            for(int j=0; j<column; j++){
+                if(arr[i][j]==value){
+                    if(j-1>=0 && i-1 >=0) sum += arr[i-1][j-1];
+                    if(j+1 < column && i-1 >=0) sum += arr[i-1][j+1];
+                    if(i+1 <row && j+1 < column) sum += arr[i+1][j+1];
+                    if(i+1 < row && j-1>=0) sum += arr[i+1][j-1];
+                }
+            }
+        }
+        return sum;
+    }
+}
+
+/**
+ * Your NeighborSum object will be instantiated and called as such:
+ * NeighborSum obj = new NeighborSum(grid);
+ * int param_1 = obj.adjacentSum(value);
+ * int param_2 = obj.diagonalSum(value);
+ */
