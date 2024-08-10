ca2ad3d4b0f991b9801334d2aa62c93886106117
Gourav Arora <gouravarora035@gmail.com>
2024-08-10 17:51:33 +0530

    Time: 1 ms (100%), Space: 41.8 MB (37.66%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -1,22 +1,27 @@
 class Solution {
     public int calPoints(String[] operations) {
-        List<Integer> list = new ArrayList<>();
-        for(String x : operations){
-            if(x.equals("+")){
-                System.out.println(x);
-                list.add(list.get(list.size()-1)+list.get(list.size()-2));
-            }else if(x.equals("D")){
-                list.add(2*(list.get(list.size()-1)));
-            }else if(x.equals("C")){
-                int index = list.size()-1;
-                list.remove(index);
-            }else{
-                list.add(Integer.parseInt(x));
+        int[] operation = new int[operations.length];
+        int index = 0;
+        for(String s : operations){
+            switch(s){
+                case "+" : operation[index] = operation[index-1] + operation[index-2];
+                            index++;
+                            break;
+                
+                case "D" : operation[index] = 2*operation[index-1];
+                            index++;
+                            break;
+
+                case "C" : index--;
+                            break;
+
+                default : operation[index] = Integer.parseInt(s);
+                            index++;
             }
         }
         int sum = 0;
-        for(int i : list){
-            sum += i;
+        for(int i =0; i<index; i++){
+            sum += operation[i];
         }
         return sum;
     }
