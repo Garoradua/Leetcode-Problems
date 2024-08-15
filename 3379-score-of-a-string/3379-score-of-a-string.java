99247d497036de228492ba14a9508ef044cd237b
Gourav Arora <gouravarora035@gmail.com>
2024-08-15 20:02:50 +0530

    Time: 1 ms (99.85%), Space: 41.9 MB (62.06%) - LeetHub

---
+++ 3379-score-of-a-string/3379-score-of-a-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int diff = 0;
+        if(s.length()<2) return (int)s.charAt(0);
+       for(int i=1; i<s.length(); i++){
+        int count1 = (int)s.charAt(i);
+        int count2 = (int)s.charAt(i-1);
+        diff += Math.abs(count1 - count2);
+       } 
+       return diff;
+    }
+}
