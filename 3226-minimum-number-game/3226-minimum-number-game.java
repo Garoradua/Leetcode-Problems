f0da816882b7608ea907ff699aa4fd3fd3fe8153
Gourav Arora <gouravarora035@gmail.com>
2024-08-22 17:52:56 +0530

    Time: 166 ms (6%), Space: 44.7 MB (61.09%) - LeetHub

--- 3226-minimum-number-game/3226-minimum-number-game.java
+++ 3226-minimum-number-game/3226-minimum-number-game.java
@@ -1,12 +1,11 @@
 class Solution {
     public int[] numberGame(int[] nums) {
-        int sum = 0;
+        // int sum = 0;
         Arrays.sort(nums);
         for(int i=0; i<nums.length; i=i+2){
-            sum = nums[i] + nums[i+1];
-            nums[i] = sum - nums[i];
-            nums[i+1] = sum - nums[i+1];
-            sum = 0;
+            nums[i] = nums[i] + nums[i+1];
+            nums[i+1] = nums[i] - nums[i+1];
+            nums[i] = nums[i] - nums[i+1];
         }
         return nums;
     }
