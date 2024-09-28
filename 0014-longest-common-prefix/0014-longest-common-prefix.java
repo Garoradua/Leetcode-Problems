002d7d9b70741b00eabdaa0c3a8e6b5176343957
Gourav Arora <gouravarora035@gmail.com>
2024-09-28 18:52:51 +0530

    Time: 1 ms (72.78%), Space: 41.3 MB (66.57%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,46 +1,14 @@
 class Solution {
-    class TrieNode{
-        TrieNode child[] = new TrieNode[26];
-        boolean isEnd;
-    }
-     TrieNode root = new TrieNode();
     public String longestCommonPrefix(String[] strs) {
-        for(int i=0; i<strs.length; i++){
-            putIntotrie(strs[i]);
-        }
-        
-        return findLongestCommonPrefix(strs[0]);
-    }
-
-    public String findLongestCommonPrefix(String str){
-        StringBuilder prefix = new StringBuilder();
-        TrieNode curr = root;
-        for(char c : str.toCharArray()){
-            int index = c-'a';
-            if(curr.child[index]!=null && countNode(curr)==1 && curr.isEnd == false){
-                curr = curr.child[index];
-                prefix.append(c);
-            }else{
-                break;
-            }
-        }
-        return prefix.toString();
-    }
-
-    public int countNode(TrieNode curr){
-        int count = 0;
-        for(int i=0; i<26; i++){
-            if(curr.child[i]!=null) count++;
-        }
-        return count;
-    }
-    public void putIntotrie(String str){
-        TrieNode curr = root;
-        for(int i=0; i<str.length(); i++){
-            int index = str.charAt(i)-'a';
-            if(curr.child[index]==null) curr.child[index] = new TrieNode();
-            curr = curr.child[index];
-        }
-        curr.isEnd = true;
+      Arrays.sort(strs);
+      String start = strs[0];
+      String end = strs[strs.length-1];
+      StringBuilder prefix = new StringBuilder();
+      int i = 0;
+      while(i<start.length() && i<end.length() && start.charAt(i)==end.charAt(i)){
+        prefix.append(start.charAt(i));
+        i++;
+      }  
+      return prefix.toString();
     }
 }
