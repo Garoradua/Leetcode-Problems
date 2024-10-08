c6360b961d03996839d8f8b46ca31bf837691cfc
Gourav Arora <gouravarora035@gmail.com>
2024-10-08 18:01:10 +0530

    Time: 1 ms (100%), Space: 43 MB (91.44%) - LeetHub

---
+++ 3055-maximum-odd-binary-number/3055-maximum-odd-binary-number.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String maximumOddBinaryNumber(String s) {
+        char[] charArray = s.toCharArray();
+        int totalLength = charArray.length;
+        int j;
+        if(charArray[totalLength-1]=='1'){
+            j = 0;
+            for(int i=0; i<totalLength-1; i++){
+                if(charArray[i]=='1'){
+                    swap(j,i, charArray);
+                    j++;
+                }
+            }
+        }else{
+            j = totalLength-1;
+            for(int i=0; i<totalLength-1; i++){
+                if(charArray[i]=='1'){
+                    swap(j,i,charArray);
+                    if(j==totalLength-1) j=0;
+                    else j++;
+                }
+            }
+        }
+        return new String(charArray);
+    }
+    public void swap(int i, int j, char[] charArray){
+        char x = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = x;
+    }
+}
