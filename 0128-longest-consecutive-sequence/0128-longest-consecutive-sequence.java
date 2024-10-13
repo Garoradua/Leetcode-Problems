c989dfe1a698229febb7996c4a89ff596fc4080b
Gourav Arora <gouravarora035@gmail.com>
2024-10-13 13:51:47 +0530

    Time: 30 ms (56.9%), Space: 67.9 MB (12.43%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,25 +1,25 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        Set<Integer> set = new TreeSet<>();
-        for(int num: nums) set.add(num);
-        int maxLength = 0;
-        int length = 1;
-        int[] arr = new int[set.size()];
-        int j = 0;
-        if(set.size()==0) return 0;
-        for(int n : set) {
-            arr[j] = n;
-            System.out.println(arr[j]);
-            j++;    
-        }
-        for(int i=1; i<set.size(); i++){
-            if(arr[i]==arr[i-1]+1) length++;
-            else{
-                maxLength = Math.max(maxLength, length);
-                length=1;
+        Set<Integer> set = new HashSet<>();
+
+        for(int n : nums) set.add(n);
+        int setSize = set.size();
+
+        if(setSize==0) return 0;
+        else if(setSize==1) return 1;
+        int maxLength = 1;
+        int count = 0;
+         int length = 1;
+        for(int n : set){
+            if(!set.contains(n-1)){
+                length = 1;
+                while(set.contains(n+length)){
+                    length++;
+                }
+
             }
+            maxLength = Math.max(maxLength, length);
         }
-        maxLength = Math.max(maxLength, length);
         return maxLength;
     }
 }
