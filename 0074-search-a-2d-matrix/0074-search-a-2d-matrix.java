be2d3d78cdd40915b4d5ec6467ab883848c1f286
Gourav Arora <gouravarora035@gmail.com>
2024-10-22 17:36:09 +0530

    Time: 0 ms (100%), Space: 41.8 MB (90.61%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,18 +1,21 @@
 class Solution {
-    public static boolean findInThisRow(int[][] matrix, int j, int m, int n, int target){
-            for(int k=0; k<n; k++){
-                if(matrix[j][k]==target) return true;
-            }
-        return false;
-    }
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        for(int i=0; i<m; i++){
-            if(matrix[i][n-1]>=target){
-                return findInThisRow(matrix, i, m, n, target);
+        int rowLength = matrix.length;
+        int colLength = matrix[0].length;
+
+        for(int i=0; i<rowLength; i++){
+            if(target <= matrix[i][colLength-1]){
+               return findInRow(i, colLength, matrix, target);
             }
         }
+
+        return false;
+    }
+    public boolean findInRow(int row, int colLength, int[][] matrix, int target){
+        for(int i=colLength-1; i>=0; i--){
+            if(matrix[row][i]==target) return true;
+        }
+
         return false;
     }
 }
