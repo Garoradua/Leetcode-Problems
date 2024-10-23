9345a3806d57da2f781e13279ff29c649788c1e8
Gourav Arora <gouravarora035@gmail.com>
2024-10-23 16:51:56 +0530

    Time: 0 ms (100%), Space: 45.8 MB (68.49%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,32 +1,50 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+        int[] result = new int[2];
+        
+        result[0] = firstOccurence(nums, target);
+        result[1] = secondOccurence(nums, target);
+
+        return result;
+    }
+
+    public int firstOccurence(int[] nums, int target){
         int start = 0;
         int end = nums.length-1;
-        int[] result = new int[2];
+        int result = -1;
         while(start<=end){
             int mid = start + (end-start)/2;
 
             if(nums[mid]==target){
-                int smaller = mid;
-                while(smaller>start && target==nums[smaller-1]){
-                    smaller--;
-                }
-                result[0] = smaller;
-                int larger = mid; 
-                while(larger<end && target==nums[larger+1]){
-                    larger++;
-                }
-                result[1] = larger;
-
-                return result;
-
-            }else if(nums[mid]>target){
+                result = mid;
+                end = mid-1;
+            }else if(nums[mid] > target){
                 end = mid-1;
             }else{
                 start = mid+1;
             }
         }
 
-        return new int[]{-1,-1};
+        return result;
+    }
+
+    public int secondOccurence(int[] nums, int target){
+        int start = 0;
+        int end = nums.length-1;
+        int result = -1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+
+            if(nums[mid]==target){
+                result = mid;
+                start = mid+1;
+            }else if(nums[mid] > target){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+
+        return result;
     }
 }
