500d16138fcd653bd6aefcbcbe260f124c563bcd
Gourav Arora <gouravarora035@gmail.com>
2024-10-24 17:07:39 +0530

    Time: 0 ms (100%), Space: 41 MB (36.2%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -5,18 +5,27 @@ class Solution {
         for(int i=0; i<n; i++){
             dp[i] = -1;
         }
-        return solveRec(nums, n-1, dp);
+        return solveRec(nums, n, dp);
     }
 
     public int solveRec(int[] nums, int n, int[] dp){
-        if(n==0) return nums[0];
+        int prev = nums[0];
+        int prev2  = 0 ;
 
-        if(n<0) return 0;
-        if(dp[n]!=-1) return dp[n];
-        int include = nums[n] + solveRec(nums, n-2, dp);
-        int exculde = 0 + solveRec(nums, n-1, dp);
-        dp[n] = Math.max(include, exculde);
+        for(int i=1; i<n; i++){
+            int inc = nums[i] + prev2;
+            int exc = 0 + prev;
+            prev2 = prev;
+            prev = Math.max(inc, exc);
+        }
 
-        return dp[n];
+        return prev;
+
+        // if(n<0) return 0;
+        // if(dp[n]!=-1) return dp[n];
+        // int include = nums[n] + solveRec(nums, n-2, dp);
+        // int exculde = 0 + solveRec(nums, n-1, dp);
+        // dp[n] = Math.max(include, exculde);
+
     }
 }
