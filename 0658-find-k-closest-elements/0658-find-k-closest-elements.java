c5d6dfe6a910eb79f25c56c8f99dae5a48c49de5
Gourav Arora <gouravarora035@gmail.com>
2024-10-24 22:34:59 +0530

    Time: 4 ms (96.32%), Space: 45.6 MB (76.19%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,37 +1,20 @@
-class Pair implements Comparable<Pair>{
-    int key;
-    int value;
-
-    Pair(int key, int value){
-        this.key = key;
-        this.value = value;
-    }
-
-    @Override
-    public int compareTo(Pair p){
-        if(this.key==p.key){
-            return Integer.compare(this.value, p.value);
-        }else{
-            return Integer.compare(this.key, p.key);
-        }
-    }
-}
-
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        PriorityQueue<Pair> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-
-        for(int i=0; i<arr.length; i++){
-            int z = Math.abs(x-arr[i]);
-            maxHeap.add(new Pair(z, arr[i]));
-            if(maxHeap.size()> k) maxHeap.poll();
-        }
+        int low = 0;
+        int high = arr.length-1;
+        int i = 0;
         List<Integer> list = new ArrayList<>();
-        while(!maxHeap.isEmpty()){
-           Pair p = maxHeap.poll();
-           list.add(p.value);
+        while(high-low >=k){
+            if(Math.abs(arr[low]-x) > Math.abs(arr[high]-x)){
+                low++;
+            }else{
+                high--;
+            }
         }
-        Collections.sort(list);
+        for( i=low; i<=high; i++)
+        list.add(arr[i]);
+
+
         return list;
     }
 }
