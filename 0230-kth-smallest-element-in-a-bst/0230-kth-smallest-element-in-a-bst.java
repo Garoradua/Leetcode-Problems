09cfa43083c35f1612db389468cce589c697e7bb
Gourav Arora <gouravarora035@gmail.com>
2024-10-24 19:42:07 +0530

    Time: 0 ms (100%), Space: 44.5 MB (36%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -15,19 +15,19 @@
  */
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
-        int kth[] = {k};
+        int[] kth = {k};
         int[] val = {-1};
-        return inOrder(root, kth, val);
+        return inOrderTraversal(root, kth, val);
     }
 
-    public int inOrder(TreeNode root, int[] kth, int[] val){
+    public int inOrderTraversal(TreeNode root, int[] kth, int[] val){
         if(root==null) return -1;
-        if(inOrder(root.left, kth, val) != -1) return val[0];
+        if(inOrderTraversal(root.left, kth, val)!=-1) return val[0];
         kth[0]--;
         if(kth[0]==0) {
             val[0] = root.val;
             return val[0];
         }
-        return inOrder(root.right, kth, val);
+        return inOrderTraversal(root.right, kth, val);
     }
 }
