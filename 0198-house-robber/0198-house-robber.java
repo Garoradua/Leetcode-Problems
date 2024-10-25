522979c9277b59b265450f57e13947b5ccbaa526
Gourav Arora <gouravarora035@gmail.com>
2024-10-25 16:58:42 +0530

    Time: 0 ms (100%), Space: 41.3 MB (9.68%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,30 +2,19 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         int[] dp = new int[n];
-        for(int i=0; i<n; i++){
-            dp[i] = -1;
-        }
-        return solveRec(nums, n, dp);
+        for(int i=0; i<n; i++) dp[i] = -1;
+        return solveRec(nums, n-1, dp);
     }
 
     public int solveRec(int[] nums, int n, int[] dp){
-        int prev = nums[0];
-        int prev2  = 0 ;
+        if(n==0) return nums[0];
 
-        for(int i=1; i<n; i++){
-            int inc = nums[i] + prev2;
-            int exc = 0 + prev;
-            prev2 = prev;
-            prev = Math.max(inc, exc);
-        }
-
-        return prev;
-
-        // if(n<0) return 0;
-        // if(dp[n]!=-1) return dp[n];
-        // int include = nums[n] + solveRec(nums, n-2, dp);
-        // int exculde = 0 + solveRec(nums, n-1, dp);
-        // dp[n] = Math.max(include, exculde);
-
+        if(n<0) return 0;
+        if(dp[n] != -1) return dp[n];
+        int include = nums[n] + solveRec(nums, n-2, dp);
+        int exclude = 0 + solveRec(nums, n-1, dp);
+        dp[n] = Math.max(include, exclude);
+        return dp[n];
     }
+        
 }
