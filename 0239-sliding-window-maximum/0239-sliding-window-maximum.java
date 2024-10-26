2fab14f973012fdfd301e2d942bbd47496ebd092
Gourav Arora <gouravarora035@gmail.com>
2024-10-26 15:45:03 +0530

    Time: 30 ms (77%), Space: 58.7 MB (59.2%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,22 +1,18 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        Deque<Integer> queue = new ArrayDeque<>();
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i=0; i<nums.length; i++){
-           while(!queue.isEmpty() && nums[queue.getLast()] < nums[i]){
+       Deque<Integer> queue  = new ArrayDeque<>();
+       int n = nums.length;
+       int[] result = new int[n-k+1];
+       int j=0;
+       for(int i=0; i<n; i++){
+            while(!queue.isEmpty() && nums[i] > nums[queue.getLast()]){
                 queue.removeLast();
-           }
-           queue.addLast(i);
-           if(i-k >= queue.getFirst()) queue.removeFirst();
-           if(i >= k-1) list.add(nums[queue.getFirst()]);
-        }
-
-        int[] result = new int[list.size()];
-
-        for(int i=0; i<list.size(); i++){
-            result[i] = list.get(i);
-        }
-
-        return result;
+            }
+            queue.add(i);
+            if(i-k >= queue.getFirst()) queue.removeFirst();
+            if(i >= k-1) result[j++] = nums[queue.getFirst()];
+            
+       } 
+       return result;
     }
 }
