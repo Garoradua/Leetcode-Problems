17e9c28891a6a979cd01ed6fbd8afd05d21f6389
Gourav Arora <gouravarora035@gmail.com>
2024-10-28 10:36:17 +0530

    Time: 0 ms (100%), Space: 40.4 MB (39.69%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,14 +2,15 @@ class Solution {
     public int climbStairs(int n) {
         int[] dp = new int[n+1];
         Arrays.fill(dp, -1);
-      return solveRec(n, dp);  
+        return solveRec(n, dp);
     }
 
     public int solveRec(int n, int[] dp){
+        // if(n<0) return 0;
         if(n==0) return 1;
         if(n==1) return 1;
         if(dp[n]!=-1) return dp[n];
-        dp[n] = solveRec(n-1, dp) + solveRec(n-2, dp);
-        return dp[n];
+       dp[n] =  solveRec(n-1, dp) + solveRec(n-2, dp);
+       return dp[n];
     }
 }
