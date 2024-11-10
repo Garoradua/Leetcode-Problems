f9f545416ee0492f999eedb515ae4ff5a52f1028
Gourav Arora <gouravarora035@gmail.com>
2024-11-10 11:11:11 +0530

    Time: 1 ms (23.99%), Space: 44.9 MB (6.58%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -16,20 +16,18 @@
 class Solution {
     public boolean isValidBST(TreeNode root) {
         ArrayList<Integer> list = new ArrayList<>();
-        long[] previous = {Long.MIN_VALUE};
-        boolean first = true;
-        return inOrderTraversal(root, list, previous);
-        // for(int i=1; i<list.size(); i++){
-        //     if(list.get(i-1)>=list.get(i)) return false;
-        // }
-        // return true;
-    }
-    public boolean inOrderTraversal(TreeNode root, List<Integer> list, long[] previous){
-        if(root==null) return true;;
-        if(inOrderTraversal(root.left, list, previous)==false) return false;
-        if(root.val <= previous[0]) return false;
-            previous[0] = root.val;
-        return inOrderTraversal(root.right, list, previous);
+        inOrderTraversal(root, list);
 
+        for(int i=1; i<list.size(); i++){
+            if(list.get(i) <= list.get(i-1)) return false;
+        }
+
+        return true;
+    }
+    public void inOrderTraversal(TreeNode root, ArrayList<Integer> list){
+        if(root==null) return;
+        inOrderTraversal(root.left, list);
+        list.add(root.val);
+        inOrderTraversal(root.right, list);
     }
 }
