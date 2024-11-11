93e4984eac640e7916cbd890d5c0eab69c66b7b7
Gourav Arora <gouravarora035@gmail.com>
2024-11-11 22:05:18 +0530

    Time: 36 ms (100%), Space: 44.7 MB (12.12%) - LeetHub

--- 1331-path-with-maximum-gold/1331-path-with-maximum-gold.java
+++ 1331-path-with-maximum-gold/1331-path-with-maximum-gold.java
@@ -1,12 +1,10 @@
 class Solution {
-    int m;
-    int n;
-    int totalGold = 0;
     public int getMaximumGold(int[][] grid) {
-        m = grid.length;
-        n = grid[0].length;
-        int maxGold = 0;
+        int m = grid.length;
+        int n = grid[0].length;
         boolean[][] visited = new boolean[m][n];
+        int maximumGold = 0;
+        int totalGold = 0;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 totalGold += grid[i][j];
@@ -14,30 +12,25 @@ class Solution {
         }
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(grid[i][j]!=0){
-                    maxGold = Math.max(maxGold, dfs(i, j, grid, visited));
-                    if(maxGold==totalGold) return totalGold;
-                }  
+                if(grid[i][j]!=0)
+                maximumGold = Math.max(findMaxGoldPath(grid, i, j, visited), maximumGold);
+                if(totalGold == maximumGold) return totalGold;
             }
         }
-        return maxGold;
-       
+        return maximumGold;
+        
     }
 
-    public int dfs(int row, int col, int[][] grid, boolean[][] visited){
-        if(row>=m || row<0 || col<0 || col>=n || visited[row][col] || grid[row][col] == 0){
-            return 0;
-        }
-
-        visited[row][col]=true;
-        int[][] allDir = new int[][]{{-1,0}, {0, 1}, {1,0}, {0, -1}};
+    public static int findMaxGoldPath(int[][] grid, int i, int j, boolean[][] visited){
+       
+       if(i<0 || j<0 || i>= grid.length || j>=grid[0].length || visited[i][j] || grid[i][j]==0) return 0;
         int maxGold = 0;
-        for(int i=0; i<4; i++){
-            int nextRow = row + allDir[i][0];
-            int nextCol = col + allDir[i][1];
-            maxGold = Math.max(maxGold, dfs(nextRow, nextCol, grid, visited));
+        visited[i][j] = true;
+        int[][] direction = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+        for(int dir=0; dir<direction.length; dir++){
+            maxGold = Math.max(findMaxGoldPath(grid, i+direction[dir][0], j+direction[dir][1], visited), maxGold);
         }
-        visited[row][col]=false;
-        return maxGold + grid[row][col];
+        visited[i][j] = false;
+        return maxGold + grid[i][j];
     }
 }
