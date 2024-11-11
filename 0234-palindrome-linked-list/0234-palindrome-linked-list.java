80d212f41fc09c351a1bcd9c5fa3d5110087d3ff
Gourav Arora <gouravarora035@gmail.com>
2024-11-11 15:21:10 +0530

    Time: 4 ms (84.42%), Space: 68.9 MB (20.39%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,21 +10,59 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        if(head==null) return false;
-        ListNode curr = head;
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        while(curr!=null){
-            list.add(curr.val);
-            curr = curr.next;
-        }
-        int left = 0;
-        int right = list.size()-1;
+        // if(head==null) return false;
+        // ListNode curr = head;
+        // ArrayList<Integer> list = new ArrayList<Integer>();
+        // while(curr!=null){
+        //     list.add(curr.val);
+        //     curr = curr.next;
+        // }
+        // int left = 0;
+        // int right = list.size()-1;
 
-        while(right >= left){
-            if(list.get(left) != list.get(right)) return false;
-            left++;
-            right--;
+        // while(right >= left){
+        //     if(list.get(left) != list.get(right)) return false;
+        //     left++;
+        //     right--;
+        // }
+        // return true;
+
+        ListNode slowPointer = head;
+        ListNode fastPointer = head;
+
+        while(fastPointer!=null && fastPointer.next!=null){
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
         }
-        return true;
+
+        ListNode secondHalf = reverse(slowPointer);
+        ListNode firstHalf = head;
+        boolean isPallindrome = true;
+        while(secondHalf!=null){
+            if(firstHalf.val != secondHalf.val) {
+                isPallindrome =  false;
+                break;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        reverse(secondHalf);
+        return isPallindrome;
+
+    }
+
+    public ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode nextnode = null;
+        ListNode curr = head;
+
+        while(curr!=null){
+            nextnode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextnode;
+        }
+        return prev;
     }
 }
