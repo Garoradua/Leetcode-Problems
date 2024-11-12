cbdfc25dde99ac16f1a06baa8edef04767cef545
Gourav Arora <gouravarora035@gmail.com>
2024-11-12 21:04:37 +0530

    Time: 51 ms (29.66%), Space: 114.5 MB (53.71%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -4,34 +4,28 @@ class Node{
     Node next;
     Node prev;
 
-    Node(int key, int value){
+    Node(int key, int val){
         this.key = key;
-        this.value = value;
+        this.value = val;
     }
-
 }
-
 class LRUCache {
+    Node head = new Node(0,0);
+    Node tail = new Node(0,0);
+    int capacity;
     Map<Integer, Node> map;
-    Node head= new Node(0, 0);
-    Node tail = new Node(0, 0);
-    int capacity = 0;;
     public LRUCache(int capacity) {
         map = new HashMap<>();
+        this.capacity = capacity;
         head.next = tail;
         tail.prev = head;
-        this.capacity = capacity;
     }
     
     public int get(int key) {
         if(map.containsKey(key)){
             Node data = map.get(key);
-            //remove
             removeNode(data);
-
-            //add
             addNode(data);
-
             return data.value;
         }
         return -1;
@@ -39,22 +33,24 @@ class LRUCache {
     
     public void put(int key, int value) {
         if(map.containsKey(key)){
-             //remove
-             Node data = map.get(key);
-              removeNode(data);
+            Node data = map.get(key);
+
+            //remove
+            removeNode(data);
+
         }
-        if(map.size() == capacity){
+        if(map.size()==capacity){
             removeNode(tail.prev);
         }
-        //add
-        addNode(new Node(key, value));
-        
+            addNode(new Node(key, value));
     }
+
     public void removeNode(Node data){
         map.remove(data.key);
-        data.prev.next = data.next;
         data.next.prev = data.prev;
+        data.prev.next = data.next;
     }
+
     public void addNode(Node data){
         map.put(data.key, data);
         data.next = head.next;
